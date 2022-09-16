d31acea51e1e7f5f123642325980191ae13831d9
jeremij94 <jeremij94@gmail.com>
2022-09-16 11:04:20 -0500

    added exercise 1 and completed exercise 2

---
+++ OthereIterationStatementsExercises/LukeWarm Section.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace OthereIterationStatementsExercises
+{
+    public class LukeWarm_Section
+    {
+        public static void LukeWarmSection()
+        {
+
+            int part1 = -1000;
+            var firstListOfNumbers = new List<int>();
+            for (part1 = -1000; part1 <= 1000; part1++);
+            {
+                Console.WriteLine($"{part1}");
+                firstListOfNumbers.Add(part1);
+
+            }
+
+
+       
+
+        }
+    }
+}
+
---
+++ OthereIterationStatementsExercises/Program.cs
@@ -0,0 +1,96 @@
+﻿
+//EXERCISE 1:
+//Write this for loop 15 times
+//Comment out when complete
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
+
+//or(int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"{i}");
+
+//}
+
+//
+
+
